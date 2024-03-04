3c498bb939b340bf51892100945ed7d73d2f9de9
evan17458 <a0952987429@yahoo.com.tw>
2024-03-04 20:49:05 +0800

    homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,28 +36,26 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+      <div className={styles.containerFlex}>
+        <div className={styles.topcontainer}>
           <img src="/img/hero.png" />
         </div>
 
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
-          <h1 className="text-4xl md:text-6xl font-bold">
-            打造數位體驗，設計明天。
-          </h1>
+        <div className={styles.downcontainer}>
+          <h1 className={styles.text}>打造數位體驗，設計明天。</h1>
 
-          <p className="md:text-xl">
+          <p className={styles.test2}>
             歡迎來到我的數位畫布，這裡有創新和創造力 收斂。
             憑藉對美學的敏銳洞察力和對程式碼的掌握，我的
             作品集展示了反映我的多樣化項目集合 追求卓越的承諾。
           </p>
           {/* BUTTONS */}
-          <div className="w-full flex gap-4">
+          {/* <div className="w-full flex gap-4">
             <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
               查看我的工作
             </button>
             <button className="p-4 rounded-lg ring-1 ring-black">聯絡我</button>
-          </div>
+          </div> */}
         </div>
       </div>
       {/* <HomepageHeader />
